fd5b5eae8187b27c2060ec310a101439f3e7ab79
Danissimmo <danila20032302@yandex.ru>
2025-07-07 18:39:25 +0300

    add colors

--- lib/ui_klt/foundation/colors_foundation.dart
+++ lib/ui_klt/foundation/colors_foundation.dart
@@ -4,10 +4,11 @@ class ColorsFoundation {
   static const Color surfaceBase = Color(0xFFFFFFFF);
   static const Color lightSurface = Color(0xFFF3F6F6);
   static const Color lightSurface2 = Color(0xFFF2F7FB);
+  static const Color darkNeutral = Color(0xFF000E08);
+  static const Color backHelloPage = Color(0xFF1A1A1A);
   static const Color backNeutral1 = Color(0xFF797C7B);
   static const Color backNeutral2 = Color(0xFF9A9E9C);
   static const Color backNeutral3 = Color(0xFFCDD1D0);
-  static const Color darkNeutral = Color(0xFF000E08);
   static const Color primary = Color(0xFF24786D);
   static const Color primary2 = Color(0xFF20A090);
   static const Color primary3 = Color(0xFF8DEEE2);
@@ -21,6 +22,8 @@ class ColorsFoundation {
   static const Color success = Colors.green;
   static const Color info = Colors.blue;
   static const Color transparent = Colors.transparent;
+  static const Color purpleLinearColor1 = Color(0xFF43116A);
+  static const Color purpleLinearColor2 = Color(0xFF0A1832);
 
   ColorsFoundation._();
 }
