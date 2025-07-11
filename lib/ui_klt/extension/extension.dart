c6d8befd27c39a61f13900b66e754c0300f29d1f
Danissimmo <danila20032302@yandex.ru>
2025-07-11 15:03:49 +0300

    fix

--- lib/ui_klt/extension/extension.dart
+++
@@ -1,6 +0,0 @@
-import 'package:chat_pet/theme/ui_kit_theme_data.dart';
-import 'package:flutter/material.dart';
-
-extension UiKitThemeExtention on BuildContext {
-  UiKitThemeData? get theme => Theme.of(this).extension<UiKitThemeData>();
-}
--- lib/ui_klt/logger/logger.dart
+++
@@ -1,6 +0,0 @@
-// ignore: depend_on_referenced_packages
-import 'package:logger/logger.dart';
-
-final log = Logger(printer: PrettyPrinter());
-
-final logNoStack = Logger(printer: PrettyPrinter(methodCount: 0));
