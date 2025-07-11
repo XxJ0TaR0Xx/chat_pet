a4f7d6e33321f635985ee6019cfe832c54a210b4
Danissimmo <danila20032302@yandex.ru>
2025-07-11 15:01:13 +0300

    add localization

---
+++ lib/localization/intl/messages_ru.dart
@@ -0,0 +1,41 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ru locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ru';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "Chatbox": MessageLookupByLibrary.simpleMessage("Chatbox"),
+    "ConnectFriendsEasilyQuickly": MessageLookupByLibrary.simpleMessage(
+      "Легко и быстро подключайте друзей",
+    ),
+    "ExistingAccount": MessageLookupByLibrary.simpleMessage(
+      "Аккаунт уже есть?",
+    ),
+    "LogIn": MessageLookupByLibrary.simpleMessage("Залогиниться"),
+    "Or": MessageLookupByLibrary.simpleMessage("Или"),
+    "OurChatAppIsThePerfectWay": MessageLookupByLibrary.simpleMessage(
+      "Наше приложение для общения в чате - это идеальный способ оставаться на связи с друзьями и семьей.",
+    ),
+    "SignUpWithMail": MessageLookupByLibrary.simpleMessage(
+      "Регистрация с почтой",
+    ),
+  };
+}
